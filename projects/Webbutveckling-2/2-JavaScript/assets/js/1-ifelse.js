c1745fa975b34c8afedd5b267ac5c64ea384563e
limpan.landin@gmail.com <ll0009@ad.lystkom.se>
2020-09-23 11:50:20 +0200

    Continued working with Js

--- projects/Webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
+++ projects/Webbutveckling-2/2-JavaScript/assets/js/1-ifelse.js
@@ -22,3 +22,33 @@ if (isSunny == true) {
 } else {
     console.log('Put on some warm clothes today'); 
 }
+
+// Step 1
+var d = new Date();
+if (d.getHours() < 18) {
+    console.log('Good Day!')
+} else {
+    console.log('Good Ebening!')
+}
+
+// Step 2
+if (d.getHours() < 10) {
+    console.log('Good morning!'); 
+} else if (d.getHours() < 18) {
+    console.log('Good Day!');
+} else {
+    console.log('Good Ebening!')
+}
+
+// Step 3
+if (d.getHours() < 10) {
+    if(d.getMonth() > 9 || d.getMonth() < 2) {
+        console.log('Go ahead and sleep like a bear!')
+    } else {
+        console.log('Good morning')
+    }
+} else if (d.getHours() < 18) {
+    console.log('Good Day!');
+} else {
+    console.log('Good Ebening!')
+}
