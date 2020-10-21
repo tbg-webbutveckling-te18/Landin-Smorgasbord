216fdcdc09f6c5e301c4fb2fb195212c2242ca0d
Linus Landin <linus.landin@edu.lycksele.se>
2020-10-21 11:52:35 +0200

    Scopes, dom manipulation and higher order functons

--- projects/Webbutveckling-2/2-JavaScript/assets/js/3-functions.js
+++ projects/Webbutveckling-2/2-JavaScript/assets/js/3-functions.js
@@ -1,16 +1,16 @@
-document.writeln('Blinka lilla stjärna där <br>');
-document.writeln('hur jag undrar var du är <br>');
-document.writeln('Fjärran lockar du min syn <br>');
-document.writeln('lik en diamant i skyn <br>');
+// document.writeln('Blinka lilla stjärna där <br>');
+// document.writeln('hur jag undrar var du är <br>');
+// document.writeln('Fjärran lockar du min syn <br>');
+// document.writeln('lik en diamant i skyn <br>');
 
-document.writeln('<br>')
+// document.writeln('<br>')
 
-document.writeln('Blinka lilla stjärna där <br>');
-document.writeln('hur jag undrar var du är <br>');
-document.writeln('Fjärran lockar du min syn <br>');
-document.writeln('lik en diamant i skyn <br>');
+// document.writeln('Blinka lilla stjärna där <br>');
+// document.writeln('hur jag undrar var du är <br>');
+// document.writeln('Fjärran lockar du min syn <br>');
+// document.writeln('lik en diamant i skyn <br>');
 
-document.writeln('<br>')
+// document.writeln('<br>')
 
 function twinkle() {
     document.writeln('Blinka lilla stjärna där <br>');
@@ -21,8 +21,8 @@ function twinkle() {
     document.writeln('<br>')
 }
 
-twinkle();
-twinkle();
+// twinkle();
+// twinkle();
 
 function square(num) {
     document.writeln(num * num);
@@ -82,3 +82,56 @@ function kebabToSnake(kebabString) {
 }
 
 document.writeln(kebabToSnake('<br>'+ 'Kebab-På-Spett'))
+
+var p = document.getElementById('scopes');
+
+// declare
+function localScope() {
+    var localVar = 31;
+    p.innerHTML = localVar;
+}
+
+// call
+localScope();
+
+var globalVar = 'This is a global variable';
+
+p.innerHTML = globalVar;
+p.style.fontSize = '20px';
+p.style.color = '#639';
+
+function changeGlobalVar() {
+    globalVar = 'Ha ha I got a new value now!';
+    p.innerHTML = globalVar;
+}
+
+changeGlobalVar();
+
+function tricky() {
+    var globalVar = 'Am I a local?';
+    p.innerHTML = globalVar;
+}
+
+tricky();
+p.innerHTML = globalVar;
+
+function myTimer() {
+    var d = new Date();
+    var clock = document.getElementById('clock');
+    clock.innerHTML = d.toLocaleTimeString();
+}
+
+myTimer();
+
+var seconds = setInterval(myTimer, 1000); 
+var btnStopTime = document.getElementsByClassName('stopTime')[0];
+var doomsStr = document.getElementById('doomsMessage');
+
+btnStopTime.addEventListener('click', function() {
+    clearInterval(seconds)
+    doomsStr.innerHTML = 'Ha Ha! Time ded';
+    doomsStr.style.transition = 'all 0.4s ease';
+    doomsStr.style.fontSize = '60px';
+    doomsStr.style.color = 'crimson';
+
+})
