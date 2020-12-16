69a646a2c4bcbbaf5d302b355a0c926b0efd5f7f
Linus Landin <linus.landin@edu.lycksele.se>
2020-12-16 11:49:09 +0100

    continued with javascript

--- projects/Webbutveckling-2/3-Eget-Projekt/assets/js/array.js
+++ projects/Webbutveckling-2/3-Eget-Projekt/assets/js/array.js
@@ -1,5 +1,18 @@
 var food = ["Pizza", "Steak", "Fish N' Chips"]
 
-function btnArray(){
-    document.getElementById("arrayResult").innerHTML = food;
+var i;
+
+for (var i = 0; i < 4; i++) {
+    document.getElementById('arrayResult').innerHTML = food[i]
 }
+
+var btnResult = document.getElementsByClassName('btnResult')[0];
+var arrayResult = document.getElementById('arrayResult');
+
+btnResult.addEventListener('click', function() {
+    var i;
+
+    for (var i = 0; i < 4; i++) {
+    document.getElementById('arrayResult').innerHTML = food[i]
+}
+})
