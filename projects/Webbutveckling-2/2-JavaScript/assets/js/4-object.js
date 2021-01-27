2c5f76f5dbdcd58ba60ff1464c275ed6ed16c065
Linus Landin <linus.landin@edu.lycksele.se>
2021-01-27 11:47:03 +0100

    More work on Objects

--- projects/Webbutveckling-2/2-JavaScript/assets/js/4-object.js
+++ projects/Webbutveckling-2/2-JavaScript/assets/js/4-object.js
@@ -1,22 +1,57 @@
 var person = ['Sid', 33, 'Male', 'EagleShieldBay'];
-
+ 
 person.shift();
-
 document.writeln(person[2])
-
-var personFriends = ['Bob', 'Dylan', 'Aize', 'Dize'];
-
+ 
+var personFriends = ['Bob', 'Dylan', 'Aiizee', 'Deezee'];
+ 
 // Perfect use case of an object
 var user = {
     name: 'The kid',
-    age: 33,
     city: 'HereSneezedDuck',
-    friends: ['Bob', 'Dylan', 'Aize', 'Dize'],
+    age: 33,
+    friends: ['Bob', 'Dylan', 'Aiizee', 'Deezee'],
     isEvil: false,
     shoutout: function() {
-        alert(`Shoutout to y'all`)
-    }
+        alert(`Shoutout to y'all!`);
+    } 
 };
+ 
+document.writeln(`<br>My name is ${user.name} and I am ${user.age} and I live in ${user.city}`);
+// user.shoutout();
+ 
+for(var i = 0; i < user.friends.length; i++) {
+    console.log(user.friends[i]);
+}
+ 
+// use forEach if array
+user.friends.forEach(function(friend) {
+    console.log(friend)
+})
+ 
+console.log(user.age)
+user.age++;
+console.log(user.age)
 
-document.writeln(`<br>My name is ${user.name} and I am ${user.age} and I live in ${user.city}`)
-user.shoutout();
+var posts = [
+    {
+        author: "Carl-Jan",
+        comment: "Hey catz rules!"
+    },
+    {
+        author: "Arnold",
+        comment: "Noway dude! Dogz rules!"
+    },
+    {
+        author: "Kamala",
+        comment: "Calm down. Both are equally nice"
+    }
+]
+
+//Html Reference
+var postOutput = document.querySelector(".posts")
+
+
+posts.forEach(function(post) {
+    postOutput.innerHTML += `Author: ${post.author} <br> Commented: ${post.comment} <br><br>`;
+})
