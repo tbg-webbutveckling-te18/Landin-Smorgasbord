e3dab8a1daa49351a4742fd6df6d2d8d9097297c
Linus Landin <linus.landin@edu.lycksele.se>
2021-06-02 11:44:00 +0200

    Got the slideshow working

--- projects/Webbutveckling-2/5-Slutprojekt/assets/js/slideshow.js
+++ projects/Webbutveckling-2/5-Slutprojekt/assets/js/slideshow.js
@@ -2,10 +2,27 @@ var arrowLeft = document.getElementsByClassName('leftArrow')[0];
 var arrowRight = document.getElementsByClassName('rightArrow')[0];
 var slider = document.getElementsByClassName('profileParent')[0];
 
+var counter = -1000
+
 arrowLeft.addEventListener('click', function(){
-    slider.style.left = '-300px'
+    counter += -300
+
+    slider.style.left = counter + 'px'
+
+    if (counter <= -3000) {
+        counter = -1000
+    }
+    console.log(counter)
 })
 
 arrowRight.addEventListener('click', function(){
-    slider.style.left = '+300px'
+    counter += 300
+
+    slider.style.left = counter + 'px'
+
+    if (counter >= 800) {
+        counter = -1000
+    }
+
+    console.log(counter)
 })
